3ad5fc7ceb31827f44f0b174d1a7fa8c52370f68
sedoy113 <sedoy113@yandex.ru>
2025-07-14 18:56:47 +0300

    add

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import './pages/index.scss';
 // Импортируем все SVG из папки icons
 const iconsContext = require.context('./icons', false, /\.svg$/);
 iconsContext.keys().forEach(iconsContext);
-import './images/logo.svg';
-import './images/avatar.jpg';
-import './images/edit-avatar-icon.svg';
 import './components/card.js';
 
