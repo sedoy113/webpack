0803de08330d8a4a48fb3f66c560fd14cc31b7fd
sedoy113 <sedoy113@yandex.ru>
2025-07-15 19:17:47 +0300

    fix @function and @import in scss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './pages/index.scss';
+import './styles/main.scss';
+
 // Импортируем все SVG из папки icons
 const iconsContext = require.context('./icons', false, /\.svg$/);
 iconsContext.keys().forEach(iconsContext);
--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const sass = require('sass');
 
 module.exports = {
-	entry: { main: './src/index.js' },
+	entry: {
+		main: './src/index.js',
+		styles: './src/styles/main.scss' // Отдельная точка входа для стилей
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-		publicPath: '',
+		filename: 'js/[name].[contenthash].js',
+		publicPath: '/',
+		assetModuleFilename: 'assets/[hash][ext][query]'
 	},
 	mode: 'development',
+	devtool: 'eval-source-map',
 	devServer: {
 		static: {
 			directory: path.resolve(__dirname, 'dist'),
-			watch: true,
 		},
 		compress: true,
 		port: 8080,
 		open: true,
 		hot: true,
-		liveReload: true,
-		watchFiles: ['src/**/*.html', 'src/**/*.scss'],
+		historyApiFallback: true,
+		watchFiles: ['src/**/*'],
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: '/node_modules/',
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						cacheDirectory: true,
+					},
+				},
 			},
-			// Правило для SVG-спрайтов (иконки в папке `src/icons/`)
 			{
 				test: /\.svg$/,
-				include: path.resolve(__dirname, 'src/icons'), // обрабатываем только SVG из папки icons
+				include: path.resolve(__dirname, 'src/icons'),
 				use: [
 					{
 						loader: 'svg-sprite-loader',
 						options: {
-							extract: true, // извлекает спрайт в отдельный файл
-							spriteFilename: 'sprite.svg', // имя файла
+							extract: true,
+							spriteFilename: 'sprite.svg',
 							symbolId: 'icon-[name]',
 						},
 					},
@@ -48,53 +58,89 @@ module.exports = {
 						loader: 'svgo-loader',
 						options: {
 							plugins: [
-								{ name: 'removeViewBox', active: false }, // не удалять viewBox
-								{ name: 'removeTitle', active: true },    // удалять <title>
-								{ name: 'removeDesc', active: true },     // удалять <desc>
-								{ name: 'removeUselessStrokeAndFill', active: true }, // удалять лишние атрибуты
+								{ name: 'removeViewBox', active: false },
+								{ name: 'removeTitle', active: true },
+								{ name: 'removeDesc', active: true },
+								{ name: 'removeUselessStrokeAndFill', active: true },
 							],
 						},
 					},
 				],
 			},
-			// Обычные SVG (не для спрайтов)
 			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/,
-				exclude: path.resolve(__dirname, 'src/icons'), // исключаем иконки для спрайтов
+				test: /\.(png|jpg|jpeg|gif|webp|avif|svg)$/i,
+				exclude: path.resolve(__dirname, 'src/icons'),
 				type: 'asset/resource',
 				generator: {
-					filename: 'images/[name][ext]',
+					filename: 'images/[name].[hash][ext]',
 				},
 			},
 			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
 				type: 'asset/resource',
 				generator: {
-					filename: 'fonts/[name][ext]',
+					filename: 'fonts/[name].[hash][ext]',
 				},
 			},
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.(sa|sc|c)ss$/i,
 				use: [
 					MiniCssExtractPlugin.loader,
 					{
 						loader: 'css-loader',
-						options: { importLoaders: 1 },
+						options: {
+							importLoaders: 2,
+							sourceMap: true,
+							url: {
+								filter: (url) => !url.includes('sprite.svg'),
+							},
+						},
 					},
-					'postcss-loader',
-					'sass-loader',
-				],
-			},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							postcssOptions: {
+								plugins: [
+									require('autoprefixer')({
+										overrideBrowserslist: ['last 2 versions', '> 1%'],
+									}),
+									require('cssnano')({
+										preset: ['default', {
+											discardComments: {
+												removeAll: true,
+											},
+										}]
+									}),
+								],
+							},
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							implementation: sass,
 
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: { importLoaders: 1 },
+							additionalData: (content, loaderContext) => {
+								if (loaderContext.resourcePath.endsWith('.scss')) {
+									return `
+                    @use "sass:math";
+                    @use "sass:color";
+                    @use "sass:map";
+                    @use "sass:list";
+                    @use "sass:meta";
+                    @use "sass:string";
+                    @use "variables" as *;
+                    @use "mixins" as *;
+                    @use "functions" as *;
+                    ${content}
+                  `;
+								}
+								return content;
+							},
+						},
 					},
-					'postcss-loader',
 				],
 			},
 		],
@@ -102,9 +148,47 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
+			filename: 'index.html',
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+			},
+			chunks: ['main', 'styles'], // Явное указание chunks
 		}),
 		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin(),
-		new SpriteLoaderPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash].css',
+			chunkFilename: 'css/[id].[contenthash].css',
+		}),
+		new SpriteLoaderPlugin({
+			plainSprite: true,
+		}),
 	],
+	resolve: {
+		alias: {
+			'@scss': path.resolve(__dirname, 'src/scss'),
+			'@images': path.resolve(__dirname, 'src/images'),
+		},
+		extensions: ['.js', '.scss', '.css'],
+	},
+	optimization: {
+		moduleIds: 'deterministic',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+				styles: {
+					name: 'styles',
+					test: /\.css$/,
+					chunks: 'all',
+					enforce: true,
+				},
+			},
+		},
+	},
 };
